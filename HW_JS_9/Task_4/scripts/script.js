3ea762c85ab99c58c5593b0056ff3b978ba334dc
HutorAlex <alexgutor@ukr.net>
2024-11-22 22:55:22 +0100

    create HW_9

---
+++ HW_JS_9/Task_4/scripts/script.js
@@ -0,0 +1,26 @@
+// Kx1xgoKy8
+// - Є масив
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
+//     Завдання робити через цикли.
+for (const course of coursesAndDurationArray) {
+
+
+     const courseDiv = document.createElement('div');
+
+     const courseH1 = document.createElement('h1');
+     courseH1.textContent = course.title;
+     const courseP = document.createElement('p');
+     courseP.textContent = course.monthDuration;
+    courseDiv.append(courseH1, courseP);
+    document.body.appendChild(courseDiv);
+
+}
